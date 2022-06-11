52cd581d7a5adf4196b34d61dd5bbde5913cf13e
Victoria <vmsoki@gmail.com>
2022-06-11 21:26:42 +0300

    digital_clock

---
+++ projects/digital_clock.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+####################
+# name: Victoria
+#file: operations.py
+#date: 6/6/2022
+######################
+
+from tkinter import *
+from tkinter.ttk import *
+from time import strftime 
+
+#creating a window
+window = Tk()
+window.title('Digital clock')
+
+def time():
+    string = strftime('%H :%M : %S %p')
+    lbl.config(text = string)
+    lbl.after(600, time)
+
+lbl = Label(window, font = ('calibri', 30 , 'bold'),
+            background = 'blue',
+            foreground = 'white')
+
+lbl.pack(anchor = 'center')
+time()
+
+mainloop()
