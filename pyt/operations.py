db5a1dc02b59fe1bb269ea26952a806209a67893
Victoria <vmsoki@gmail.com>
2022-06-13 09:12:39 +0300

    operations

---
+++ pyt/operations.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+####################
+# name: Victoria
+#file: operations.py
+#date: 6/6/2022
+######################
+def add_numbers(x,y):
+    result=x+y
+    return result
+
+def mult_numbers(x,y):
+    result=x*y
+    return result
+
+def sub_numbers(x,y):
+    result=x-y
+    return result
+
+def div_numbers(x,y):
+    result=x/y
+    return result
+
+
+
+
+
+
+
+
+#you can write diffrent codes but run them together by using the import function to the main pyhton function.
