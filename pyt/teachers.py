5302c5fb0f9b63d73d45dcd47848968a917c7485
Victoria <vmsoki@gmail.com>
2022-06-13 09:17:48 +0300

    teachers

---
+++ pyt/teachers.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+####################
+# name: Victoria
+#file: operations.py
+#date: 6/6/2022
+######################
+
+class Teacher:
+    def __init__(self,name,tsc_no,subject,salary):
+        self.name=name
+        self.tsc_no=tsc_no
+        self.subject=subject
+        self.salary=salary
+    def get_tsc_no(tsc_no):
+        return tsc_no
