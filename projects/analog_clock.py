4c820fa1d06cd4623581b3a75c5c673d5ae1dfd4
Victoria <vmsoki@gmail.com>
2022-07-04 11:10:45 +0300

    index

--- projects/analog_clock.py
+++ projects/analog_clock.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python
 
-####################
-# name: Victoria
-#file: analog_clock.py
-#date: 6/6/2022
-######################
+'''
+ name: Victoria
+file: analog_clock.py
+date: 6/6/2022
+'''
 
 from PyQt5.QtWidgets import *
 from PyQt5 import QtCore , QtGui
@@ -13,7 +13,7 @@ from PyQt5.QtCore import *
 import sys
 
 class clock(QMainWindow):
-    def__init__(self):
+    def __init__(self):
         super().__init__()
         timer = QTimer(self)
         timer.timeout.connect(self.update)
@@ -32,3 +32,41 @@ class clock(QMainWindow):
                                   QPoint(0,-90)])#CREATING THE SECOND HAND
         self.bColor = Qt.blue # minute and hour hand colors
         self.sColor = Qt.yellow #second hand color
+
+    def paintEvent(self, event):
+        rec = min(self.width(), self.height())
+        tik= QTime.currentTme() # getting the current time
+        painter =QPainter(self)
+
+        def drawPointer(color, rotation, pointer):
+            painter.setBrush(QBrush(color))
+            painter.save()
+            painter.rotate(rotation)
+            painter.drawConvexPolygon(pointer)
+            painter.restore()
+
+        painter.setRenderHint(QPainter.Antialiasing)
+        painter.translate(self.width()/2,self.height()/2)
+        painter.scale(rec/300,rec/300)
+        painter.setPen(QtCore.Qt.NoPen)
+
+        #drawing each hand
+        drawPointer(self.bColor, (30 * (tik.hour() + tik.minute()/60)),self.hPointer)
+        drawPointer(self.bColor, (6 * (tik.minute() + tik.second()/60)),self.mPointer)
+        drawPointer(self.sColor, (6 * tik.second()),self.sPointer)
+
+        painter.setPen(QPen(self.bColor))
+
+        for i in range (0,60):
+
+            if(i%5) == 0:
+                painter.drawLine(87,0,97,0)
+            painter.rotate(6)
+        painter.end()
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    win = Clock()
+    win.show()
+    exit(app.exec_())
+
+
